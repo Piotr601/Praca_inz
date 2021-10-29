b2efa889fe601bf300709492b2d5ca56cacaeeac
Piotr <piotr.niedziolka601@gmail.com>
2021-10-29 16:33:35 +0200

    v0.1

---
+++ Program/program.py
@@ -0,0 +1,28 @@
+# Imie / Name: Piotr Niedziolka
+# Nr indeksu / Index number: 249023
+# Praca inzynierska / Engineering Thesis
+
+# Libraries:
+import thinkdsp as T_DSP
+import thinkplot as T_PLOT
+
+name = 'Piotr'
+
+
+def introduction():
+    # Introduction
+    print(f'Hi, ' + name)
+
+
+def processing():
+    # Here all function/information about ThinkDSP
+    sinus_signal = T_DSP.SinSignal(freq=1)
+    T_PLOT.config(xlabel="time(s)", legend=False)
+    wave = sinus_signal.make_wave(duration=1, framerate=11025)
+    wave.plot()
+    T_PLOT.show()
+
+
+if __name__ == '__main__':
+    introduction()
+    processing()
