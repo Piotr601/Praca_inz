a2b8845b5cf87132a026cd94e2321300c68bc2a5
Piotr <piotr.niedziolka601@gmail.com>
2021-10-29 15:19:03 +0200

    v0.3

--- Program/program.py
+++ Program/program.py
@@ -8,22 +8,26 @@ import thinkplot as T_PLOT
 
 name = 'Piotr'
 
-
+# Introduction, basic informations
 def introduction():
-    # Introduction
     print(f'Hi, ' + name)
 
 
-def processing():
-    # Here all function/information about ThinkDSP
+# Loading audio to program
+def audio_loading():
+    print('Loading')
+
+
+# Function, basic information to analysis
+def processing(): 
     sinus_signal = T_DSP.SinSignal(freq=1)
     T_PLOT.config(xlabel="time(s)", legend=False)
     wave = sinus_signal.make_wave(duration=1, framerate=11025)
     wave.plot()
     T_PLOT.show()
 
-
+# Main function
 if __name__ == '__main__':
-    # Main function
     introduction()
+    audio_loading()
     processing()
