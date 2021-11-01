8f7331ac8e53aa5e17a21a8bba5b7fc51a1d4eda
Piotr <piotr.niedziolka601@gmail.com>
2021-11-01 16:42:56 +0100

    v0.4

--- Program/program.py
+++ Program/program.py
@@ -5,29 +5,58 @@
 # Libraries:
 import thinkdsp as T_DSP
 import thinkplot as T_PLOT
+from tkinter import *
 
-name = 'Piotr'
+#window = Tk()
+# Audio start
+A_start = 0
+# Audio end
+A_end = 5
+
+author = 'Piotr'
 
 # Introduction, basic informations
 def introduction():
-    print(f'Hi, ' + name)
-
+    print(f'> Hi, ' + author) 
 
 # Loading audio to program
-def audio_loading():
-    print('Loading')
+def processing():
+    print(' >>>> Loading <<<< ')
 
+    name = input("Podaj nazwę pliku: ")
+    print(name)
+
+    audio =  T_DSP.read_wave(name + ".wav")
+    audio.plot()
+
+    T_PLOT.config(xlim=[A_start, A_end], xlabel="time(s)", legend=False)
+    T_PLOT.show()
+
+    audio_spectrum=audio.make_spectrum()
+    audio_spectrum.plot()
+    T_PLOT.show()
 
 # Function, basic information to analysis
-def processing(): 
-    sinus_signal = T_DSP.SinSignal(freq=1)
-    T_PLOT.config(xlabel="time(s)", legend=False)
-    wave = sinus_signal.make_wave(duration=1, framerate=11025)
-    wave.plot()
-    T_PLOT.show()
+# def example(): 
+#    sinus_signal = T_DSP.SinSignal(freq=1)
+#    
+#    wave = sinus_signal.make_wave(duration=1, framerate=11025)
+#    wave.plot()
+#
+#    T_PLOT.config(xlabel="-", legend=False)
+#    T_PLOT.show()
+
 
 # Main function
 if __name__ == '__main__':
+
+    #window.title("Program do analizy")
+    #window.geometry("1000x800+50+50")
+    #window.resizable(False, False)
+    #window.mainloop()
+
     introduction()
-    audio_loading()
     processing()
+ #   example()
+
+    
